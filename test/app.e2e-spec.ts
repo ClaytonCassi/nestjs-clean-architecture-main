399778476e0a595dd76e8d7a0f14e94ebde287f4
Clayton Cassi <69650975+ClaytonCassi@users.noreply.github.com>
2022-12-09 16:40:39 -0300

    Update app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+//end to end test file
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
