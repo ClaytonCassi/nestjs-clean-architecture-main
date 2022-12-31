815b93f1eca24bd0760f7122cb8c94e5b4d1dbbf
Clayton Cassi <69650975+ClaytonCassi@users.noreply.github.com>
2022-12-31 15:50:43 -0300

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//teste file
 describe('AppController', () => {
   let appController: AppController;
 
