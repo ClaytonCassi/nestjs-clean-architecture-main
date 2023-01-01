6a12ef051d2183ad568324829b99f67eed6d4975
Clayton Cassi <69650975+ClaytonCassi@users.noreply.github.com>
2023-01-01 15:04:51 -0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-//app 
+
 @Controller('prefixo')
 export class AppController {
   constructor(private readonly appService: AppService) {}
